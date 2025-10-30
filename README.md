d7b8f1206b6a38d08dcad11d1651b926cc24d8ac
Jao_Santin <vitor-santin@hotmail.com>
2025-10-30 20:17:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+  - Aplicativo que vai conseguir receber arquivos de uma máquina de pontos digital(AFD - ARQUIVO FONTE DE DADOS) e manipular para entender quantas horas trabalhadas/descansadas de cada pessoa.
+  - Terá a função de fazer correções manuais de horários e preencher dados de trabalhadores, tendo todos os dados de qualquer pessoa. Exemplo: Aniversário, Tempo de empresa, Vestuário, Dados pessoais, Tempo trabalhado, Dias crédito/débito, Atestados.
+  - Funcao de calendário mostrando informações mais importantes relacionadas à funcionarios. Quem faltou, quem atrasou etc.
+
+  ---
+
+**Dados importantes para a realização do trabalho.**
+ *****EM CASO DE DUVIDA PEGAR OS DADOS DA PORTARIA 671, TEM EM PDF*****
+
+ - AFD = Arquivo Fonte de dados.
+ 
+ Ele e feito por um arquivo de texto composto com linhas que mostra o histórico do que aconteceu em minha máquina de pontos. Tem-se 9 tipos diferentes de registro, sendo assim:
+
+1- cabecalho
+2- inclusao ou alteracao da identificacao da empresa
+3- marcacao de pontos
+4- ajuste do relogio
+5- inclusao, alteracao ou exclusao de empregado no rep
+6- eventos sensíveis(abertura/violacao de rep)
+7- marcacao de pontos
+9- trailer(mostra a quantidade de registros realizados para cada tipo no rep)
+
+
---
+++ src/main.rs
@@ -0,0 +1,376 @@
+use std::{fs, fmt, path::PathBuf};
+use rfd::FileDialog;
+use encoding_rs::WINDOWS_1252; // ou ISO_8859_1, se preferir
+use iced::{Element, Task as Command};
+use iced::widget::{column, button, text};
+use iced::{Alignment::{Center}};
+//
+enum Screens{
+    Main,
+    // LobbyColab,
+    // Calendar
+}
+// enum DecodeTypes{
+//     WinUTF
+// }
+struct AFDBase{
+    nsr: &str,
+    tipo: RegistryTypes
+    
+}
+//voltar aqui
+struct Cabecalho{
+    base: AFDBase,
+    tipo_empregador: str,
+    cnpj_empregador: str,
+    cno_empregador: str,
+    razao_social: str,
+    id_rep: str,
+    data_inicio: str,
+    data_final: str,
+    geracao_arquivo: str,
+    versao_leiaute_afd: str,
+    cnpj_fabricante_rep: str,
+    modelo_rep: str,
+    registro_hexa: str,
+}
+
+struct CreateUpdateEmpresa{
+    base: AFDBase
+
+}
+struct MarcacaoPonto{
+    base: AFDBase
+
+}
+struct AjusteRelogio{
+    base: AFDBase
+
+}
+struct CreateaUpdateDeleteEmpregado{
+    base: AFDBase
+
+}
+struct SensivelREP{
+    base: AFDBase
+
+}
+struct MarcacaoPontoRepP{
+    base: AFDBase
+
+}
+struct Trailer{
+    base: AFDBase
+
+}
+
+enum RegistryTypes{
+    Cabecalho,
+    CreateUpdateEmpresa,
+    MarcacaoPonto,
+    AjusteRelogio,
+    CreateUpdateDeleteEmpregado,
+    SensivelREP,
+    MarcacaoPontoRepP,
+    Trailer,
+}
+
+impl RegistryTypes{
+    fn get_type_by_number(number: i8)->Option<Self>{
+        match number{
+            1 => Some(Self::Cabecalho),
+            2 => Some(Self::CreateUpdateEmpresa),
+            3 => Some(Self::MarcacaoPonto),
+            4 => Some(Self::AjusteRelogio),
+            5 => Some(Self::CreateUpdateDeleteEmpregado),
+            6 => Some(Self::SensivelREP),
+            7 => Some(Self::MarcacaoPontoRepP),
+            9 => Some(Self::Trailer),
+            _ => None,
+        }
+    }
+    fn parse(&self, linha: &str){
+        match self{
+            Self::Cabecalho => {
+                let n_serie = &linha[0..9];
+                println!("numero de serie: {}", n_serie);
+                let tipo = self;
+                println!("{}", tipo);
+                let mut tipo_empregador_txt = String::new();
+                if let Some(char_casa_11) = linha.chars().nth(10){
+                    let tipo_empregador = char_casa_11.to_digit(10).unwrap() as i8;
+                    if tipo_empregador == 1{
+                        tipo_empregador_txt = "CNPJ".to_string()
+                    }else{
+                        tipo_empregador_txt = "CPF".to_string()
+                    };
+                    println!("tipo_empregador: {}", tipo_empregador_txt)
+
+                }
+                let cnpj_cpf = &linha[11..25];
+                println!("cnpj/cpj: {}", cnpj_cpf);
+                let cno_caepf = &linha[25..39];
+                println!("cno: {}", cno_caepf);
+                let razao_social = &linha[39..189];
+                println!("razao_social: {}", razao_social.to_string().trim());
+                let id_rep = &linha[189..206];
+                println!("id_rep: {}", id_rep);
+
+                let data_inicio = &linha[206..216];
+                println!("data_inicio: {}", data_inicio);
+
+                let data_final = &linha[216..226];
+                println!("data_final: {}", data_final);
+
+                let geracao_arquivo = &linha[226..250];
+                println!("data/hora geracao arquivo: {}", geracao_arquivo);
+
+                let versao_leiaute_afd = &linha[250..253];
+                println!("leiaute_afd: {}", versao_leiaute_afd);
+
+                let tipo_fabricante_rep_char = &linha[253..254];
+                let tipo_fabricante_rep = if tipo_fabricante_rep_char.to_string() == "1"{"CNPJ"}else{"CPF"};
+                println!("Tipo fabricante: {}", tipo_fabricante_rep);
+
+                let cnpj_fabricante_rep = &linha[254..268];
+                println!("CNPJ fabricante rep: {}", cnpj_fabricante_rep.to_string().trim());
+
+                let modelo_rep = &linha[268..298];
+                println!("Modelo REP: {}", modelo_rep.to_string().trim());
+
+                let registro_hexa = &linha[298..302];
+                println!("Registro hexa: {}", registro_hexa)
+            },
+
+            Self::CreateUpdateEmpresa => { //2
+                let nsr = &linha[0..9];
+                let registro = self;
+                let date_time = &linha[10..34];
+                let cpf_admin = &linha[34..48];
+                let tipo_empregador_txt = &linha[48..49];
+                let tipo_empregador_int: i8 = tipo_empregador_txt.parse::<i8>().unwrap();
+                let tipo_empregador = match tipo_empregador_int{
+                    1 => "CNPJ",
+                    2 => "CPF",
+                    _ => "NAO LISTADO",
+
+                };
+                let cnpj_empregador = &linha[49..63];
+                let cno = &linha[63..77];
+                let razao_social = &linha[77..227];
+                let local_servico = &linha[227..327];
+                let registro_hexa = &linha[327..332];
+
+                println!("nsr: {}", nsr);
+                println!("registro: {}", registro);
+                println!("date time: {}", date_time);
+                println!("cpf_admin: {}", cpf_admin);
+                println!("tipo empregador: {}", tipo_empregador);
+                println!("cnpj empregador: {}", cnpj_empregador.trim());
+                println!("cno: {}", cno);
+                println!("razao_social: {}", razao_social);
+                println!("local_servico: {}", local_servico);
+                println!("registro_hexa: {}", registro_hexa);
+            },
+            
+            Self::MarcacaoPonto => { //3
+                let nsr = &linha[0..9];
+                let registro = self;
+                let date_time = &linha[10..34];
+                let cpf_empregado = &linha[35..46];
+                let registro_hexa = &linha[46..50];
+                println!("nsr: {}", nsr);
+                println!("registro: {}", registro);
+                println!("data/hora: {}", date_time);
+                println!("cpf empregado: {}", cpf_empregado);
+                println!("registro hexa: {}", registro_hexa)
+            },
+
+            Self::AjusteRelogio => { //4
+                let nsr = &linha[0..9];
+                let registro = self;
+                let date_time_antes_registro = &linha[10..34];
+                let date_time_ajustado = &linha[34..58];
+                let cpf_adm = &linha[58..69];
+                let registro_hexa = &linha[69..73];
+                println!("nsr: {}", nsr);
+                println!("registro: {}", registro);
+                println!("date_time_antes_registro: {}", date_time_antes_registro);
+                println!("date time ajustado: {}", date_time_ajustado);
+                println!("cpf adm: {}", cpf_adm);
+                println!("registro_hexa: {}", registro_hexa);
+
+            },
+            Self::CreateUpdateDeleteEmpregado => { //5
+                let nsr = &linha[0..9];
+                let registro = self;
+                let date_time = &linha[10..34];
+                let tipo_operacao_str = &linha[34..35];
+                let tipo_operacao = match tipo_operacao_str{
+                    "I" => "Inclusao",
+                    "A" => "Alteracao",
+                    "E" => "Exclusao",
+                    _ => "Evento nao listado"
+                };
+                let cpf_empregado = &linha[36..47];
+                let nome_empregado = &linha[47..99];
+                let mais_dados_empregado = &linha[100..104];
+                let cpf_adm = &linha[104..115];
+                let registro_hexa = &linha[115..118];
+                println!("nsr: {}", nsr);
+                println!("registro: {}", registro);
+                println!("data/hora: {}", date_time);
+                println!("tipo operacao: {}", tipo_operacao);
+                println!("cpf empregado: {}", cpf_empregado);
+                println!("nome empregado: {}", nome_empregado.trim());
+                println!("mais_dados_empregado: {}", mais_dados_empregado);
+                println!("cpf admin: {}", cpf_adm);
+                println!("registro hexa: {}", registro_hexa);
+
+            },
+
+            Self::SensivelREP=> { //6
+                let nsr = &linha[0..9];
+                let registro = self;
+                let date_time = &linha[10..34];
+                let tipo_evento_str = &linha[34..36];
+                let tipo_evento_int: i8 = tipo_evento_str.parse::<i8>().unwrap();
+                let evento = match tipo_evento_int{
+                    1 => "Abertura/Violacao REP",
+                    2 => "Retorno Energia",
+                    3 => "Introducao de Pendrive",
+                    4 => "Retirada de Pendrive",
+                    5 => "Emissao da Relacao de Marcacoes",
+                    6 => "Erro de impressão",
+                    7 => "Disponibilidade de Servico",
+                    8 => "Indisponibilidade de Servico",
+                    _ => "Evento nao listado"
+                };
+                println!("nsr: {}", nsr);
+                println!("registro: {}", registro);
+                println!("data/hora: {}", date_time);
+                println!("tipo evento: {} -> {}", tipo_evento_str, evento)
+            },
+            
+            Self::MarcacaoPontoRepP => println!("TODO"),//7
+            Self::Trailer => println!("TODO"),//9
+        }
+    }
+}
+impl fmt::Display for RegistryTypes{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        let s= match self{
+            Self::Cabecalho => "Cabecalho",
+            Self::CreateUpdateEmpresa => "CreateUpdateEmpresa",
+            Self::MarcacaoPonto => "MarcacaoPonto",
+            Self::AjusteRelogio => "AjusteRelogio",
+            Self::CreateUpdateDeleteEmpregado=> "CreateUpdateDeleteEmpregado",
+            Self::SensivelREP => "SensivelREP",
+            Self::MarcacaoPontoRepP => "MarcacaoPontoRepP",
+            Self::Trailer => "Trailer", 
+        };
+        write!(f, "{}", s)
+    }
+}
+
+struct InterfaceRH{
+    screen: Screens
+}
+
+#[derive(Debug, Clone)]
+enum Buttons{
+    GetAFDFile
+}
+
+#[derive(Debug, Clone)]
+enum Message{
+    ButtonPressed(Buttons)
+}
+
+impl Default for InterfaceRH{
+    fn default() -> Self{
+        Self{
+            screen: Screens::Main
+        }
+    }
+
+}
+
+impl InterfaceRH{
+    fn decode_from_win1252_to_utf8(path: PathBuf){
+        match fs::read(&path) {
+            Ok(bytes) => {
+                // Tenta decodificar de Windows-1252 para UTF-8
+                let (conteudo, _, _) = WINDOWS_1252.decode(&bytes);
+                println!("Conteúdo decodificado:\n{}", conteudo);
+
+                // Aqui você pode começar a fazer o parse:
+                for linha in conteudo.lines() {
+                    InterfaceRH::parse_rep_line(linha)
+                }
+            }
+            Err(e) => eprintln!("Erro ao ler o arquivo: {}", e),
+        }
+
+    }
+    fn parse_rep_line(linha: &str){
+        println!("*--*--*--*--*--*--*--*--*--*--*");
+        println!("linha: {}", linha);
+        println!("----");
+        if let Some(c) = linha.chars().nth(9){
+            let n: i8 = c.to_digit(10).unwrap_or(0) as i8;
+            if let Some(tipo_registry) = RegistryTypes::get_type_by_number(n){
+                tipo_registry.parse(linha)
+            }else{
+                println!("nao tem tipo com esse numero...")
+            }
+
+        }else{
+            println!("Sem caractere na posicao 10")
+        }
+
+    }
+    fn update(&mut self, message: Message) -> Command<Message>{
+        match message{
+            Message::ButtonPressed(button) => {
+                match button{
+                    Buttons::GetAFDFile => {
+                        if let Some(path) = FileDialog::new()
+                            .add_filter("Arquivos de texto", &["txt"])
+                            .set_title("SELECIONE O ARQUIVO DE PONTO!")
+                            .pick_file()
+                        {
+                            InterfaceRH::decode_from_win1252_to_utf8(path);
+
+                        } else {
+                            println!("Nenhum arquivo selecionado.");
+                        }
+                    }
+                }
+                Command::none()
+            }
+        }
+    }
+    fn view(&self) -> Element<'_, Message> {
+        match &self.screen{
+            Screens::Main => {
+                column![
+                    text("qualquer coisa"),
+                    button("GetAFDFile")
+                        .on_press(Message::ButtonPressed(Buttons::GetAFDFile))
+                ].align_x(Center).into()
+            }
+        }
+    }
+
+}
+
+
+fn main() -> iced::Result{
+    dotenv::dotenv().ok();
+    iced::application("Interface RH", InterfaceRH::update, InterfaceRH::view)
+        .window_size((800.0, 600.0))
+        .run()
+}
+// fn main() {
+// }
